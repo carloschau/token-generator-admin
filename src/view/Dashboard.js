69d8bf87a4c4b46a0c6c2e2141c052791cf2c3df
Carlos Chau <carlos.chau719@gmail.com>
2020-10-28 18:16:34 +0800

    Show project list in dashboard

--- src/view/Dashboard.js
+++ src/view/Dashboard.js
@@ -1,9 +1,47 @@
 import * as React from "react";
-import { Card, CardContent, CardHeader } from '@material-ui/core';
+import { Card, CardContent, CardHeader, Grid } from '@material-ui/core';
 
-export default () => (
-    <Card>
-        <CardHeader title={"Welcome to the administration"} />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
-);
+import { Title } from 'react-admin';
+
+class dashboard extends React.Component {
+
+    constructor(prop){
+        console.log("constructor")
+        super(prop)
+        this.state = { projects: []}
+        
+        
+    }
+    
+    componentDidMount(){
+        console.log("componentDidMount");
+        const authToken = localStorage.getItem('auth')
+        const request  = new Request(process.env.REACT_APP_API_URL + '/projects', {
+            method: 'GET',
+            headers: new Headers({ 
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authToken}` 
+            })
+        });
+        fetch(request)
+            .then(response => response.json())
+            .then(response => {
+                console.log("state update")
+                this.setState({
+                    projects: response
+                })
+            })
+    }
+
+    render(){
+        console.log("Render dashboard");
+        return (<Grid container spacing={2} justify="center" alignItems="flex-start" direction="row">
+            {this.state.projects.map(p => {
+                return (<Card item><CardContent>{p.name}</CardContent></Card>)
+            })}
+        </Grid> )
+
+    }
+
+}
+export default dashboard;            
